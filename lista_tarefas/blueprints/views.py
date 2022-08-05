a8895479cb1c0fc3af24a792ada498de20f77d9b
Engels Souza <engels.franca@icloud.com>
2022-08-05 00:34:27 -0300

    adicionado os primeiros endpoints GET e POST

--- lista_tarefas/blueprints/views.py
+++ lista_tarefas/blueprints/views.py
@@ -1,7 +1,25 @@
-from flask import Blueprint
+import json
+from flask import Blueprint, jsonify, request, url_for
 
 api = Blueprint('api', __name__)
 
-@api.route('/')
-def index():
-    return "<h1>Flask Funcional!</h1>"
+lst = [
+    {'id': 0, 'tarefa': "Lavar os pratos"},
+    {'id': 1, 'tarefa': "Arrumar o quarto"},
+    {'id': 2, 'tarefa': "cozinhar"}
+]
+
+
+@api.route('/lista/')
+def lista():
+    return jsonify(lst)
+
+@api.route('/lista/<int:id>')
+def get_lista(id):
+    return jsonify(lst[id])
+
+@api.route('/lista/', methods=['POST'])
+def update_list():
+    record = json.loads(request.data)
+    lst.append(dict(id=len(lst), tarefa=record))
+    return jsonify(json.dumps(record)), 201
